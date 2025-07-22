a40ba6aaffacd9ec6d576beed56e871d92fcfb01
Kamran <kamran.niftaliyev@eilink.az>
2025-07-22 10:27:54 +0400

    remove posts controller

--- TwoHandApp/Controllers/AdvertisementsController.cs
+++ TwoHandApp/Controllers/AdvertisementsController.cs
@@ -1,26 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
 using TwoHandApp.Models;
-using System.IO;
 
 namespace TwoHandApp.Controllers;
-//test//
 [ApiController]
 [Route("api/[controller]")]
 public class AdvertisementsController : ControllerBase
 {
     private readonly IWebHostEnvironment _environment;
-    private readonly AppDbContext context;
+    private readonly AppDbContext _context;
 
     public AdvertisementsController(IWebHostEnvironment environment, AppDbContext context)
     {
         _environment = environment;
-        this.context = context;
+        this._context = context;
     }
     [HttpGet("id/{id}")]
     public ActionResult<IEnumerable<Advertisement>> GetAdvertisement(int id)
     {
         var baseUrl = $"{Request.Scheme}://{Request.Host}";
-        var advertisements = context.Posts.FirstOrDefault(x => x.Id == id);
+        var advertisements = _context.Posts.FirstOrDefault(x => x.Id == id);
 
         if (advertisements == null)
         {
@@ -47,7 +47,7 @@ public class AdvertisementsController : ControllerBase
     public ActionResult<IEnumerable<Advertisement>> GetMyAdvertisements()
     {
         var baseUrl = $"{Request.Scheme}://{Request.Host}";
-        var advertisements = context.Advertisements.ToList().Take(4);
+        var advertisements = _context.Advertisements.ToList().Take(4);
 
         var result = advertisements.Select(ad => new
         {
@@ -70,7 +70,7 @@ public class AdvertisementsController : ControllerBase
     {
         var baseUrl = $"{Request.Scheme}://{Request.Host}";
         //var advertisements = context.Advertisements.ToList();
-        var advertisements2 = context.Posts.ToList();
+        var advertisements2 = _context.Posts.ToList();
 
         var result = advertisements2.Select(ad => new
         {
@@ -86,21 +86,81 @@ public class AdvertisementsController : ControllerBase
             //ad.VipExpiryDate,
             //ImageUrl = $"{baseUrl}/{ad.Image}"
         });
-
         return Ok(result);
     }
 
-    //[HttpGet("{imageName}")]
-    //public IActionResult GetImage(string imageName)
-    //{
-    //    var path = Path.Combine(_environment.WebRootPath, imageName);
-        
-    //    if (!System.IO.File.Exists(path))
-    //    {
-    //        path = Path.Combine(_environment.WebRootPath, "2el.Az");
-    //    }
+    [HttpPost]
+    [Consumes("multipart/form-data")]
+    public async Task<IActionResult> CreatePostWithFiles(
+    [FromForm] Post post,
+    List<IFormFile> photos)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
 
-    //    var imageFileStream = System.IO.File.OpenRead(path);
-    //    return File(imageFileStream, "image/jpeg");
+        // Проверка на уникальность номера объявления
+        var exists = await _context.Posts.AnyAsync(p => p.PostNumber == post.PostNumber);
+        if (exists)
+        {
+            return Conflict("Post with this PostNumber already exists.");
+        }
+
+        // Сохраняем файлы как URL (например, в wwwroot/images)
+        string? imagePath = null;
+        bool turn = true;
+        foreach (var photo in photos)
+        {
+            if (photo.Length > 0)
+            {
+                var fileName = Guid.NewGuid() + Path.GetExtension(photo.FileName);
+                if (turn)
+                {
+                    imagePath = fileName;
+                    var savePath1 = Path.Combine("wwwroot", fileName);
+                    using var stream1 = new FileStream(savePath1, FileMode.Create);
+                    await photo.CopyToAsync(stream1);
+
+                }
+                else
+                    turn = false;
+                var savePath = Path.Combine("wwwroot/images", fileName);
+                Directory.CreateDirectory("wwwroot/images");
+                using var stream = new FileStream(savePath, FileMode.Create);
+                await photo.CopyToAsync(stream);
+                //post.Photos.Add(new PostPhoto
+                //{
+                //    Url = "/images/" + fileName
+                //});
+            }
+        }
+        // Добавление поста
+        post.imageUrl = $"{imagePath}" ?? "default.jpg";
+        _context.Posts.Add(post);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetPost), new { id = post.Id }, post);
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Post>> GetPost(int id)
+    {
+        var post = await _context.Posts/*
+            .Include(p => p.Photos)*/
+            .FirstOrDefaultAsync(p => p.Id == id);
+
+        if (post == null)
+            return NotFound();
+
+        return post;
+    }
+    //[HttpGet("mongo/{name}")]
+    //public async Task<IActionResult> Mongo(string name)
+    //{
+    //    var client = new MongoClient("mongodb://localhost:27017");
+    //    var database = client.GetDatabase("test");
+    //    var collection = database.GetCollection<BsonDocument>("products");
+    //    var result = collection.Find("{item: 'name'}").ToList();
+    //    var jsonResults = result.Select(doc => BsonExtensionMethods.ToJson(doc));
+
+    //    return Ok(jsonResults);
     //}
 }
