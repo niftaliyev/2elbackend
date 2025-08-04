67c2c5adc6db133ed5153d85ce002bcff664e3ae
Kamran <kamran.niftaliyev@eilink.az>
2025-08-04 11:30:28 +0400

    remove db

--- TwoHandApp/Program.cs
+++ TwoHandApp/Program.cs
@@ -102,7 +102,7 @@ app.UseSwaggerUI();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>(); // db
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
 
